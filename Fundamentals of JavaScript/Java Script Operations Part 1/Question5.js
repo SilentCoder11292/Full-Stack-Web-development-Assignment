abb3936bde1f20530fe882e7e83d85c5aeca8ac2
SilentCoder11292 <surajkumar11292@gmail.com>
2024-05-22 04:31:54 +0530

    Commiting operators questions

---
+++ Fundamentals of JavaScript/Java Script Operations Part 1/Question5.js
@@ -0,0 +1,10 @@
+// Q5. Write a JavaScript program that calculates the simple interest using the formula Simple interest =
+// (principal * rate * time) / 100.
+
+const principal = 3000;
+const rate = 7; // 7 is in percent 
+let time = 2; // time in year 
+
+
+let simpleInterest = (principal*rate*time)/100;
+console.log("Simple Interest =",simpleInterest);
---
+++ Fundamentals of JavaScript/Java Script Operations Part 1/Question6.js
@@ -0,0 +1,8 @@
+// Q6. Write a Javascript program to calculate the Body Mass Index (BMI) using the formula
+//  BMI = weight (kg)/height * height.
+
+let weight = 75; //in kg
+let height = 1.67; // in m
+
+let BMI = weight / (height*height);
+console.log("BMI =", BMI);
