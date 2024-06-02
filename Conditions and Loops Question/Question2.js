b67c1b85927487cf3804b783e616a41bac67534c
SilentCoder11292 <surajkumar11292@gmail.com>
2024-06-02 05:40:27 +0530

    Consitions and loops question subbmitted

---
+++ Conditions and Loops Question/Question2.js
@@ -0,0 +1,15 @@
+const grade = 65;
+
+if (grade>90){
+    console.log("Congratulations you got 'A' grade.");
+}
+else if(grade>=70 && grade <=90){
+        console.log("You got 'B' grade, Hope you will get A grade next time.");
+}
+else if (grade >=50 && grade <70){
+    console.log("You got 'C' grade , Need to improve a lot.");
+}
+
+else{
+    console.log("You got 'F' Grade , What can i say it's time that you start to improve skills other than studies.");
+}
---
+++ Conditions and Loops Question/Question5.js
@@ -0,0 +1,15 @@
+// Part 1 in scending order 
+let i =1;
+while(i<=25){
+    console.log(i);
+    i++;
+}
+
+// Part 2 descending order
+ console.log();
+let s =25
+while(s>=1){
+    console.log(s);
+
+    s--;
+}
